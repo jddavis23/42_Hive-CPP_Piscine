0f8d190c908857edb2393baa97ea363c292e78bb
Jefferson Davis <davisjd.jefferson23@gmail.com>
2023-01-09 12:22:21 +0200

    working with abstract classes, virtual functions and std::exception

--- CPP_05/ex02/PresidentialPardonForm.cpp
+++ CPP_05/ex02/PresidentialPardonForm.cpp
@@ -6,7 +6,7 @@
 /*   By: jdavis <jdavis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 11:38:19 by jdavis            #+#    #+#             */
-/*   Updated: 2023/01/09 12:04:03 by jdavis           ###   ########.fr       */
+/*   Updated: 2023/01/09 12:22:15 by jdavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,3 +32,4 @@ PresidentialPardonForm::~PresidentialPardonForm()
 {
 
 };
+
