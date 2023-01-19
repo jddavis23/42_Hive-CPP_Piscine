b16e4cfd92c5918bfd2180ec2012ddba39e5226c
Jefferson Davis <davisjd.jefferson23@gmail.com>
2023-01-19 16:47:00 +0200

    Final project

--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jeffdavis <jeffdavis@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 15:52:56 by jeffdavis         #+#    #+#             */
-/*   Updated: 2023/01/19 16:05:13 by jeffdavis        ###   ########.fr       */
+/*   Updated: 2023/01/19 16:46:23 by jeffdavis        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,16 @@ int main()
     std::cout << mstack.top() << std::endl;
     mstack.pop();
     std::cout << mstack.size() << std::endl;
-    mstack.push(3); mstack.push(5); mstack.push(737); //[...] mstack.push(0);
+    mstack.push(3); mstack.push(5); mstack.push(737); //[...] 
+    mstack.push(0);
     MutantStack<int>::iterator it = mstack.begin(); MutantStack<int>::iterator ite = mstack.end();
     ++it;
     --it;
-    while (it != ite) {
+    while (it != ite)
+    {
         std::cout << *it << std::endl;
-    ++it; }
-    std::stack<int> s(mstack); return 0;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+    return 0;
 }
